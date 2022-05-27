eb1adaa6047246b47240283a551ea5809ebf104e
zac <isaacmubiru99@gmail.com>
2022-05-27 19:53:32 +0300

    fix default user role

--- models/userModel.js
+++ models/userModel.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['user', 'admin'],
-    default: 'user'
+    default: 'admin'
   },
   password: {
     type: String,
